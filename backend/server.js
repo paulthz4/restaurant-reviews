3629f9cd71132f81d82a1d3832b3f23d5edd97f9
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2021-11-26 15:28:02 -0700

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,13 @@
 import express from "express"
 import cors from "cors"
-import restaurants from "./api/restaurants.rout.js"
+import restaurants from "./api/restaurant.route.js"
 
-const app  = express();
+const app = express()
 
-app.use(cors())
 app.use(cors())
 app.use(express.json())
 
-app.use("/api/v1/restaurants", restaurants);
-app.use("*",(req, res) => res.status(404).json({error: "not found"}));
+app.use("/api/v1/restaurants", restaurants)
+app.use("*", (req, res) => res.status(404).json({ error: "not found"}))
 
-export default app;
+export default app
