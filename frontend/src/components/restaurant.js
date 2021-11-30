dfd2aae86d21650c3be39872f98b442676a73a3d
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2021-11-30 15:57:37 -0700

    frontend

---
+++ frontend/src/components/restaurant.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Restaurant(){
+  return(
+    <div></div>
+  );
+  
+}
+export default Restaurant;
---
+++ frontend/src/components/restaurantsList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function RestaurantsList(){
+  return(
+    <div></div>
+  );
+  
+}
+export default RestaurantsList;
